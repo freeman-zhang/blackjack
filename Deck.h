28fc176a6c714ad674c10a16c78328d0977404a6
freeman-zhang <freemanz0824@gmail.com>
2018-05-13 10:01:33 -0500

    modified a file

---
+++ Deck.h
@@ -0,0 +1,14 @@
+class Deck: public Hand{
+public:
+	Deck();
+	virtual ~Deck();
+
+	void populate();
+
+	void shuffle();
+
+	void deal(Hand& aHand);
+
+	void additionalCards(GenericPlayer& aGenericPlayer);
+
+};
